56c78ad0c0fbf94daf2fd9c1ca6c46fe89cc3c6f
Oleh Smolynets <smolynets@gmail.com>
2023-12-15 17:09:32 +0200

    rm cache and fix test fixture

--- productapp/tests/tests_db.py
+++ productapp/tests/tests_db.py
@@ -5,8 +5,8 @@ from django.core.exceptions import ValidationError
 from productapp.models import Category
 
 
-@pytest.mark.django_db
-def test_category_parent(client):
+@pytest.fixture
+def ten_nested_categories():
     category_one = Category.objects.create(name="one")
     category_two = Category.objects.create(name="two", parent=category_one)
     category_three = Category.objects.create(name="three", parent=category_two)
@@ -18,22 +18,17 @@ def test_category_parent(client):
     category_nine = Category.objects.create(name="nine", parent=category_eight)
     category_ten = Category.objects.create(name="ten", parent=category_nine)
     category_eleven = Category.objects.create(name="eleven", parent=category_ten)
+    return category_eleven
+
+
+@pytest.mark.django_db
+def test_category_parent(ten_nested_categories):
     assert Category.objects.count() == 11
 
 
 @pytest.mark.django_db
-def test_category_limit(client):
-    category_one = Category.objects.create(name="one")
-    category_two = Category.objects.create(name="two", parent=category_one)
-    category_three = Category.objects.create(name="three", parent=category_two)
-    category_four = Category.objects.create(name="four", parent=category_three)
-    category_five = Category.objects.create(name="five", parent=category_four)
-    category_six = Category.objects.create(name="six", parent=category_five)
-    category_seven = Category.objects.create(name="seven", parent=category_six)
-    category_eight = Category.objects.create(name="eight", parent=category_seven)
-    category_nine = Category.objects.create(name="nine", parent=category_eight)
-    category_ten = Category.objects.create(name="ten", parent=category_nine)
-    category_eleven = Category.objects.create(name="eleven", parent=category_ten)
+def test_category_limit(ten_nested_categories):
+    category_eleven = ten_nested_categories
     with pytest.raises(Exception) as exc_info:
         Category.objects.create(name="eleven", parent=category_eleven)
     assert exc_info.type == ValidationError
