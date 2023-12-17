fa62d054d9eee9b22d16757706ce59846403a451
Oleh Smolynets <smolynets@gmail.com>
2023-12-17 22:30:02 +0200

    add swagger

--- backend/urls.py
+++ backend/urls.py
@@ -15,13 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, re_path
+from rest_framework import permissions
 from productapp.urls import urlpatterns as backend_urlpatterns
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Productapp API",
+        default_version='v1',
+        description="Swagger API for productapp",
+        terms_of_service="https://www.productapp.com/terms/",
+        contact=openapi.Contact(email="contact@productapp.com"),
+        license=openapi.License(name="productapp License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # add new urls
